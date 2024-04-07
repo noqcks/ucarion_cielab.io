f17969663b4160df4a9097c1d02b7b87ac05612a
noqcks <benji@093b.org>
2024-04-07 03:24:38 +0000

    Upgrade dependencies

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -160,7 +160,7 @@ export default function App() {
             About this tool
           </Nav.Link>
 
-          <Form className="ml-3" inline>
+          <Form className="ml-3" inline="true">
             <Form.Label
               className={isHeaderTextWhite ? "text-light" : "text-dark"}
             >
